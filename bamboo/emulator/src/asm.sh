8a16d08a4e110c8d8cc0df669545dd2fba0f4767
Zhitov Aleksandr <azhitov@rib.kg>
2017-12-21 12:07:34 +0600

    a lot of changes

--- bamboo/emulator/src/asm.sh
+++ bamboo/emulator/src/asm.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-if [ -z "$REAL_DEVICE"]; then
-  echo "Container is using android emulator"
-else
-  echo "Starting android screen mirro..."
-  java -jar /root/asm.jar $ANDROID_HOME
-fi
+echo "Starting android screen mirro..."
+java -jar /root/asm.jar $ANDROID_HOME
+
